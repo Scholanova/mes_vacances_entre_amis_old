6f7ad194e8ae855f95c41d067cc23bcc4aa39756
Florian <florianbematol@gmail.com>
2020-06-30 16:12:34 +0200

    Update production credentials config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         username: 'postgres',
-        password: 'postgres',
+        password: '123',
         database: 'project_vacance_entre_amis_development',
         host: '127.0.0.1',
         dialect: 'postgres',
@@ -9,15 +9,15 @@ module.exports = {
     },
     test: {
         username: 'postgres',
-        password: 'postgres',
+        password: '123',
         database: 'project_vacance_entre_amis_test',
         host: '127.0.0.1',
         dialect: 'postgres',
         logging: false
     },
     production: {
-        username: 'postgres',
-        password: 'postgres',
+        username: 'vacanceentre_4537',
+        password: 'LfxT9jOm1gaG1CruWq8a',
         database: 'project_vacance_entre_amis_production',
         host: '127.0.0.1',
         dialect: 'postgres',
