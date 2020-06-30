366a570816bec280c02c6b0b6c187543e738465a
Florian <florianbematol@gmail.com>
2020-06-30 15:52:37 +0200

    Test migrations ( Creation User routes/index.js )
    Update production credentials config.js
    
    Update production database name
    
    Update config/production database url
    
    fix
    
    Update config.js
    
    Add PROCFILE
    
    fix
    
    fix
    
    fix

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         username: 'postgres',
-        password: 'postgres',
+        password: '123',
         database: 'project_vacance_entre_amis_development',
         host: '127.0.0.1',
         dialect: 'postgres',
@@ -9,17 +9,14 @@ module.exports = {
     },
     test: {
         username: 'postgres',
-        password: 'postgres',
+        password: '123',
         database: 'project_vacance_entre_amis_test',
         host: '127.0.0.1',
         dialect: 'postgres',
         logging: false
     },
     production: {
-        username: 'postgres',
-        password: 'postgres',
-        database: 'project_vacance_entre_amis_production',
-        host: '127.0.0.1',
+        use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
         logging: (msg) => console.log('[DATABASE]', msg)
     }
